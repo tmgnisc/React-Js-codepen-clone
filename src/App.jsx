80f05b6675767defa4e3ca8748f5102fd4409cf5
Nischal <tamangnischal2018@gmail.com>
2024-06-08 12:55:24 +0545

    final output

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,99 @@
-import './App.css'
+import React, { useCallback, useState } from 'react'
 import Navbar from './components/Navbar'
-import Result from './components/Result'
-import React, {useState, useEffect} from 'react'
-import {Javascript} from '@codemirror/lang-javascript'
-import {html} from '@codemirror/lang-html'
-import {css } from '@codemirror/lang-css'
-import { useCallback } from 'react'
-import CodeMirror from '@uiw/react-codemirror'
-
+import CodeMirror from "@uiw/react-codemirror";
+import { html } from '@codemirror/lang-html';
+import { css } from '@codemirror/lang-css';
+import { javascript } from '@codemirror/lang-javascript';
+import Result from './components/Result';
 
 function App() {
-  const [html_edit, setHtml_Edit] = useState('')
-  const [css_edit, setCss_Edit]= useState('')
-  const [js_edit, setJs_Edit] = useState('')
-  const onChangeHtml = useCallback((value)=> {
+  //* create three usestate 
+  const [html_edit, setHtml_Edit] = useState('');
+  const [css_edit, setCss_Edit] = useState('');
+  const [js_edit, setJs_Edit] = useState('');
+
+  //* Html onchange handler
+  const onChangeHtml = useCallback((value) => {
+    console.log(value);
     setHtml_Edit(value);
   }, [])
 
+  //* Css onchange handler 
   const onChangeCss = useCallback((value) => {
-    setCss_Edit(value);
-  }, [])
+    console.log(value);
+    setCss_Edit(value)
+  }, []);
 
+  //* JavaScript onchange handler 
   const onChangeJavaScript = useCallback((value) => {
-    setJs_Edit(value);
-  }, [])
+    console.log(value);
+    setJs_Edit(value)
+  }, []);
+
+  //* Create Html Document
+  const srcCode = `
+  <html>
+  <body>${html_edit}</body>
+  <style>${css_edit}</style>
+  <script>${js_edit}</script>
+  </html>
+  `
+
+
   return (
-  <div>
-      <Navbar/>
-      <div className='p-2'>
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-2'>
-          <div className='bg-[#282c34] p-4 rounded-lg shadow'>
-            <h2 className='text-lg font-semibold mb-2  text-white'>HTML</h2>
+    <div>
+      {/* Navbar  */}
+      <Navbar />
+
+      {/* main content  */}
+      <div className=" p-2">
+        {/* Editor  */}
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
+          {/* Html Editor */}
+          <div className="bg-[#282c34] p-4 rounded-lg shadow">
+            <h2 className="text-lg font-semibold mb-2 text-white">HTML</h2>
+            <CodeMirror
+              className="text-xl border-gray-700 border"
+              value={html_edit}
+              height="342px"
+              theme="dark"
+              extensions={[html(true)]}
+              onChange={onChangeHtml}
+            />
           </div>
-          <div className='bg-[#282c34] p-4 rounded-lg shadow'>
-            <h2 className='text-lg font-semibold mb-2 text-white'>CSS</h2>
+
+          {/* Css Editor  */}
+          <div className="bg-[#282c34] p-4 rounded-lg shadow">
+            <h2 className="text-lg font-semibold mb-2 text-white">CSS</h2>
+            <CodeMirror
+              className="text-xl border-gray-700 border"
+              value={css_edit}
+              height="342px"
+              theme="dark"
+              extensions={[css(true)]}
+              onChange={onChangeCss}
+            />
           </div>
-          <div className='bg-[#282c34] p-4 rounded-lg shadow'>
-            <h2 className='text-lg font-semibold mb-2 text-white'>Javascript</h2>
+
+          {/* JavaScript Editor  */}
+          <div className="bg-[#282c34] p-4 rounded-lg shadow">
+            <h2 className="text-lg font-semibold mb-2 text-white">JavaScript</h2>
+            <CodeMirror
+              className="text-xl border-gray-700 border"
+              value={js_edit}
+              height="342px"
+              theme="dark"
+              extensions={[javascript(true)]}
+              onChange={onChangeJavaScript}
+            />
           </div>
         </div>
+
+        <Result
+        srcCode={srcCode}
+        />
       </div>
-  </div>
+    </div>
   )
 }
 
