0d2f93c310a19e47ecbdd54cecc76f8d3047baf9
Nischal <tamangnischal2018@gmail.com>
2024-06-08 12:29:16 +0545

    navbar jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
-function Navbar(){
-    return(
-        <div>
-        <div>
-            <div>
-                <img src="" alt="" />
+function Navbar() {
+    return (
+        <div
+            className='main lg:flex md:flex flex-wrap justify-between items-center px-4 bg-[#2f3640] py-4'>
+            <div className="left">
+                <div className="logo font-bold text-2xl text-white text-center">
+                    <img className=' w-44' src="img/download.png" alt="logo" />
+                </div>
             </div>
-        </div>
         </div>
     )
 }
+
+export default Navbar
